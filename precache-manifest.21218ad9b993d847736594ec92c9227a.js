129fc5291c7297803163aacec7c03fbc3ead966e
DaryaG1 <daryamaksimova01@gmail.com>
2022-08-03 14:06:55 +0300

    Updates

---
+++ precache-manifest.21218ad9b993d847736594ec92c9227a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea0d97b6745e430247b5e2c030326161",
+    "url": "/to_do_list/index.html"
+  },
+  {
+    "revision": "a0ce01a498f00c60d615",
+    "url": "/to_do_list/static/css/main.d780d2b8.chunk.css"
+  },
+  {
+    "revision": "8687e3895d0165189ec3",
+    "url": "/to_do_list/static/js/2.38b57ddc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/to_do_list/static/js/2.38b57ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0ce01a498f00c60d615",
+    "url": "/to_do_list/static/js/main.a69490ec.chunk.js"
+  },
+  {
+    "revision": "36472d35c87203511c0f",
+    "url": "/to_do_list/static/js/runtime-main.e2447257.js"
+  }
+]);
